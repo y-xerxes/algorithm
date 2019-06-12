cf470b24d7a5c64daed1960f4ae47618301634e9
y-xerxes <18538728360@163.com>
2019-06-12 10:51:16 +0800

    2019-06-12

---
+++ src/linkedList/AddTwoNumbers.java
@@ -0,0 +1,27 @@
+public class AddTwoNumbers {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode l3 = new ListNode(0);
+        ListNode cur = l3;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int v1 = l1 == null ? 0 : l1.val;
+            int v2 = l2 == null ? 0 : l2.val;
+            int sum = v1 + v2 + carry;
+            cur.next = new ListNode(sum % 10);
+            carry = sum / 10;
+            cur = cur.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        if (carry == 1) cur.next = new ListNode(1);
+        return l3.next;
+    }
+}
---
+++ src/string/LengthOfLongestSubstring.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class LengthOfLongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        int length = s.length();
+        HashMap<Character, Integer> map = new HashMap<>();
+        int res = 0;
+        for (int i=0,j=0; i<length; i++) {
+            if (map.containsKey(s.charAt(i))) {
+                j = Math.max(map.get(s.charAt(i)), j);
+            }
+            map.put(s.charAt(i), i+1);
+            res = Math.max(res, i - j + 1);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "qwvawsrew";
+    }
+}
